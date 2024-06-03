77956be71513630afae43cc25b7419ec7b49f3ef
ZP25HK <zp25hk@cs.aau.dk@ai-fe02>
2024-06-03 13:16:02 +0200

    Double descent experiment models commited

---
+++ run_gpu.sh
@@ -0,0 +1,2 @@
+sbatch --gres=gpu:1 run_newmodel_gpu.sh
+#sbatch --gres=gpu:a40:1 run_newmodel_gpu.sh
---
+++ run_newmodel_gpu.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash -l
+#SBATCH --job-name=PAC_BAYES_DD
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --partition batch
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=64G
+#SBATCH --time=0-12:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=arma@cs.aau.dk
+
+
+nvidia-smi
+
+
+BASEFOLDER=/home/cs.aau.dk/zp25hk/PAC_Bayes_Chernoff_MLL
+PYTHON="singularity exec --nv $BASEFOLDER/containers/firstim python"
+SCRIPT=$BASEFOLDER/train_models.py
+
+$PYTHON $SCRIPT
+
+
+
